585d97ce318ab2bbba89830530928619c90598e2
radityaseptian <radityaseptian1551@gmail.com>
2023-05-20 16:08:03 +0700

    fix font size

--- src/components/HeaderAnimation.jsx
+++ src/components/HeaderAnimation.jsx
@@ -4,7 +4,7 @@ import { animation } from "../config/string"
 export default function HeaderAnimation({description}) {
   let value = animation(description)
   return (
-    <div>
+    <div className="text-lg md:text-xl">
       {value.map((item) => {
         return <span key={item}>{item} </span>
       })}
