51cc95002d4502c04e8d23726f97fe0a75d34572
radityaseptian <radityaseptian1551@gmail.com>
2023-06-01 17:11:23 +0700

    update projects

--- src/config/projects.js
+++ src/config/projects.js
@@ -1,37 +1,40 @@
 import zeitplan from '../assets/projects/zeitplan.png'
 import shiganime from '../assets/projects/shiganime.png'
+import portofolio from '../assets/projects/portofolio.png'
+import blog from '../assets/projects/blog.png'
 
 export const projects = [
   {
     title: 'Shiganime Streaming Anime',
     description: 'Website application to streaming all anime what you want',
-    build: 'React.js, TailwindCSS, Public Api',
+    build: 'React.js, TailwindCSS, Public Api, Vite',
     image: shiganime,
-    hrefRepo: 'https://github.com/radityaseptian',
+    hrefRepo: 'https://github.com/radityaseptian/shiganime',
     hrefDemo: 'https://shiganime-en.vercel.app/',
   },
   {
-    title: 'Shiganime Streaming Anime',
-    description: 'Website application to streaming all anime what you want',
-    build: 'React.js, TailwindCSS, Public Api',
-    image: shiganime,
-    hrefRepo: 'https://github.com/radityaseptian',
-    hrefDemo: 'https://shiganime-en.vercel.app/',
-  },
-  {
-    title: 'Shiganime Streaming Anime',
-    description: 'Website application to streaming all anime what you want',
-    build: 'React.js, TailwindCSS, Public Api',
+    title: 'The Zeitplan',
+    description:
+      'Slicing website design figma into landing page website,this website using design codedesign.dev',
+    build: 'React.js, TailwindCSS, Vite',
     image: zeitplan,
-    hrefRepo: 'https://github.com/radityaseptian',
-    hrefDemo: 'https://shiganime-en.vercel.app/',
+    hrefRepo: 'https://github.com/radityaseptian/landing-page',
+    hrefDemo: 'https://zeitplan-rho.vercel.app/',
   },
   {
-    title: 'Shiganime Streaming Anime',
-    description: 'Website application to streaming all anime what you want',
-    build: 'React.js, TailwindCSS, Public Api',
-    image: zeitplan,
-    hrefRepo: 'https://github.com/radityaseptian',
-    hrefDemo: 'https://shiganime-en.vercel.app/',
+    title: 'Personal Website',
+    description: 'Personal Website To Introduce Myself',
+    build: 'React.js, TailwindCSS, Vite',
+    image: portofolio,
+    hrefRepo: 'https://github.com/radityaseptian/portofolio',
+    hrefDemo: 'https://radityaseptian.my.id/',
+  },
+  {
+    title: 'Personal Blogs Website',
+    description: 'A Simple Web To Write And Share My Articles About Technology',
+    build: 'React.js, Tailwind, Vite, Express.js, MongoDB',
+    image: blog,
+    hrefRepo: '#',
+    hrefDemo: '#',
   },
 ]
