e1394fefd08021dcbf893b7f0c1fecbd4209e47b
radityaseptian <radityaseptian1551@gmail.com>
2023-06-11 13:09:44 +0700

    add resume/cv

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,6 +2,7 @@ import Navbar from '../layouts/Navbar'
 import Button from '../components/Button'
 import { AiOutlineDownload } from 'react-icons/ai'
 import { useEffect } from 'react'
+import img from '/Raditya-CV.png'
 
 export default function Resume() {
   useEffect(() => {
@@ -13,19 +14,35 @@ export default function Resume() {
       <div className='bg-primary text-white min-h-screen bg-gradient-to-t from-[#0A192F] to-black/30'>
         <Navbar />
         <section className='border-b-8 border-primary min-h-screen'>
-          <div className='py-6 px-2 container max-w-6xl mx-auto'>
+          <div className='py-6 px-2 container max-w-5xl mx-auto'>
             <center>
-              <Button>
-                Download
-                <AiOutlineDownload />
-              </Button>
+              <a
+                href='/Raditya-CV.pdf'
+                target='_blank'
+                rel='noopener noreferrer'
+                download
+              >
+                <Button>
+                  Download
+                  <AiOutlineDownload />
+                </Button>
+              </a>
             </center>
-            <div className='py-6'>{/* PDF RESUME */}</div>
+            <div className='py-6'>
+              <img src={img} alt='Loading PDF...' />
+            </div>
             <center>
-              <Button>
-                Download
-                <AiOutlineDownload />
-              </Button>
+              <a
+                href='/Raditya-CV.pdf'
+                target='_blank'
+                rel='noopener noreferrer'
+                download
+              >
+                <Button>
+                  Download
+                  <AiOutlineDownload />
+                </Button>
+              </a>
             </center>
           </div>
         </section>
